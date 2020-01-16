4e78c6bcf4357210c9ea54b06856d980dd4ada3c
JIWONKIM <qhsh866@gmail.com>
2020-01-16 11:00:34 +0900

    initial commit

--- app/src/main/java/com/example/guru_login/CardFlipActivity.kt
+++ app/src/main/java/com/example/guru_login/CardFlipActivity.kt
@@ -30,29 +30,27 @@ class CardFlipActivity : AppCompatActivity(), FragmentManager.OnBackStackChanged
 
         getFrontFragment(savedInstanceState)
 
-        //SwipeDetector.kt와 연결해줍니다.
+        //SwipeDetector.kt와 연결
         val swipeDetector = SwipeDetector()
 
         val card_flip_container = card_flip_container
 
         card_flip_container.setOnTouchListener(swipeDetector)
 
-        //온클릭리스너(여기에서 TODO!)
+        //여기에서 TODO!)
         card_flip_container.setOnClickListener {
             //스와이프
             if (swipeDetector.swipeDetected()) {
                 //스와이프 처리
                 if (swipeDetector.action.equals(SwipeDetector.Action.LR)) {
 
-                    Log.d(TAG, "signInWithEmail:success")
-                    Toast.makeText(baseContext, "왼쪽으로 슬라이드",
-                        Toast.LENGTH_SHORT).show()
-                    //여기에 적용할 코드를 입력해주세요~
+                    //왼쪽 슬라이드 시 TODO 입력
+
                 } else if (swipeDetector.action.equals(SwipeDetector.Action.RL)) {
-                    //여기에 적용할 코드를 입력해주세요~
-                    Log.d(TAG, "signInWithEmail:success")
-                    Toast.makeText(baseContext, "오른쪽으로 슬라이드",
-                        Toast.LENGTH_SHORT).show()
+
+                    //오른쪽 슬라이드 시 TODO 입력
+
+
                 }
             }else {
                 flipCard()
@@ -82,9 +80,9 @@ class CardFlipActivity : AppCompatActivity(), FragmentManager.OnBackStackChanged
             .setCustomAnimations(
                 R.animator.cardflip_right_in, R.animator.cardflip_right_out,
                 R.animator.cardflip_left_in, R.animator.cardflip_left_out)
-            // 뒷면으로 바뀜
+            // 뒷면으로
             .replace(R.id.card_flip_container, CardFlipBack())
-            // 뒤로가기 누르면 앞면을 보여줌
+            // 뒤로가기 누르면 앞면을 보여주기
             .addToBackStack(null)
             .commit();
     }
